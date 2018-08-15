94526fe994216cf6934d9dca89b3c82af9ecc001
AGCH <anastasiyache@gmail.com>
2018-08-16 00:22:48 +0300

    BookLook Ex1 Searching By Title

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-$('button').on('click', function(){
-    var AppendData = function (data) {
-      console.log(data);
-      for (let i=0; i<10; i++){
-      var source = $('#book-template').html();
-      var template = Handlebars.compile(source);
-      var newHTML = template(data.items[i]);
-      $('.book').append(newHTML);
-      }
+$('button').on('click', function () {
+  $('.book-list').empty();
+  var title = $('input').val();
+  //---First Checking entered title
+  let check= / /g;
+  if ((title.length>15)&&(!check.test(title))) {
+   alert("Very long title, insert spaces");
+   $('input').val = '';
+  }
+  
+  else
+  fetch(title);
+  })
+
+  var AppendData = function (data) {
+    //---Second Checking entered title
+    if (data.totalItems===0)
+    $('.book-list').append("Can't find book with entered title.");
+    else {
+    var source = $('#book-template').html();
+    var template = Handlebars.compile(source);
+    for (let i = 0; i < 10; i++) {
+    var newHTML = template(data.items[i]);
+    $('.book-list').append(newHTML);
     }
-    var fetch = function (title) {
-      $.ajax({
-        method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:'+ title,
-        //+ isbn_data.toString(),
-        success: function(data) {
-        AppendData(data);
-        },            
-        error: function(jqXHR, textStatus, errorThrown) {
-          console.log(textStatus);
-        }
-      }); 
-    };
-    var title = $('input').val();
-    console.log(title);
-    //console.log('https://www.googleapis.com/books/v1/volumes?q=isbn:'+ toString(isbn_data)); //0439023521
-    fetch(title);
-    
-    })
+  }
+  }
+
+  var fetch = function (title) {
+    $.ajax({
+      method: "GET",
+      url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + title,
+      //+ isbn_data.toString(),
+      success: function (data) {
+      AppendData(data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+      }
+    });
+  };
